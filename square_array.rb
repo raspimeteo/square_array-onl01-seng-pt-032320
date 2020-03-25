bd5c76e2020fc6b5aa38b69f244da8c92df6986b
John Zonneveld <jjczon@gmail.com>
2020-03-25 14:35:54 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   square_array=[]
   array.each do |element|
-    square_array << (element * element)
+    square_array << (element ** 2)
   end
 return square_array
 end
